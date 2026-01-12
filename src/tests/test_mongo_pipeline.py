e3caf5237c549131e17fd775d045dce01d06b2de
SamVorn <samuelvorn@gmail.com>
2026-01-12 17:23:04 -0500

    comments

--- src/tests/test_mongo_pipeline.py
+++ src/tests/test_mongo_pipeline.py
@@ -2,22 +2,21 @@ from pymongo import MongoClient
 from src.repository.noSQL import DocumentSurveySource  # the general adapter
 from src.pipeline import run_pipeline
 
-# --- Connect to MongoDB (still using Mongo for testing) ---
+# using mongodb for testing
 client = MongoClient("mongodb://localhost:27017")
 db = client["thesis_pipeline"]
 
-# --- Create the generic document adapter ---
+#creating the adapter
 source = DocumentSurveySource(
     template_collection=db.surveys,
     response_collection=db.responses,
     flags_collection=db.flags
 )
 
-# --- Run your pipeline ---
+#run pipeline
 rules_path = "src/rules/pii_patterns.json"
 output = run_pipeline(source, survey_id="survey1", rules_path=rules_path)
 
-# --- Print results ---
 if output.get("results"):
     print("\nFlagged PII results:")
     for res in output["results"]:
